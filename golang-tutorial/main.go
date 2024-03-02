0d3af4616ca054217edf4219a67c7dbabfb14731
TaiChi <anothai.0978452316@gmail.com>
2024-03-03 02:09:39 +0700

    initial commit about bubble sort DSA golang

---
+++ golang-tutorial/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+func main() {
+	fmt.Println("Bubble Sorting Algorithm")
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	fmt.Println("Unsorted array: ")
+	for _, v := range arr {
+		fmt.Printf("%d ", v)
+	}
+	bubbleSort(arr)
+	fmt.Println("\nSorted array: ")
+	for _, v := range arr {
+		fmt.Printf("%d ", v)
+	}
+}
