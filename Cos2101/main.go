b8c6797addab511f7ef2f6e34ec64fa0ced421cd
TaiChi <anothai.0978452316@gmail.com>
2024-04-28 23:58:12 +0700

    test:Create new type to test again for I gotten it correct

--- Cos2101/main.go
+++ Cos2101/main.go
@@ -6,16 +6,34 @@ type Person struct {
 	name string
 	age  int
 }
+type Product struct {
+	name  string
+	price float64
+	count int
+	info  string
+}
 
-func DefaultPerson1() Person {
-	return Person{
-		age: 10,
-	}
+func (pd Product) Display() {
+	fmt.Printf("Name : %s, price : %f, count : %d, info : %s\n", pd.name, pd.price, pd.count, pd.info)
 }
 func (p Person) PrintInfo() {
 	fmt.Printf("Name : %s, age : %d\n", p.name, p.age)
 }
 func main() {
+	pd := Product{
+		name:  "Apple",
+		price: 10.5,
+		count: 10,
+		info:  "good",
+	}
+	pd.Display()
+	var pd2 Product
+	pd2.name = "Banana"
+	pd2.price = 20.5
+	pd2.count = 20
+	pd2.info = "bad"
+	pd2.Display()
+
 	p := Person{name: "Tom", age: 10}
 
 	fmt.Println("Define name :", p.name)
