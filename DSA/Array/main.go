10c3b124318a1a671d2bbeba234d55c55d9f70a4
TaiChi <anothai.0978452316@gmail.com>
2024-05-25 01:33:44 +0700

    feat : new function updated value in array

--- DSA/Array/main.go
+++ DSA/Array/main.go
@@ -5,10 +5,23 @@ import "fmt"
 func addValue(myArray []int, value ...int) []int {
 	return append(myArray, value...)
 }
+func updateValue(myArray []int, value int, newValue int) []int {
+	for i := 0; i < len(myArray); i++ {
+		if myArray[i] == value {
+			myArray[i] = newValue
+		}
+	}
+	return myArray
+}
+func displayValue(myArray []int) {
+	for i := 0; i < len(myArray); i++ {
+		fmt.Println(" Value : ", myArray[i])
+	}
+}
 func main() {
 	myArray := []int{}
 	myArray = addValue(myArray, 112, 113, 334)
-	for i := 0; i < len(myArray); i++ {
-		fmt.Printf("value : %d ", myArray[i])
-	}
+	displayValue(myArray)
+	updateValue(myArray, 113, 118)
+	displayValue(myArray)
 }
