65f9fd00619c13ea471b7e8144fd4f412e0621f4
TaiChi <anothai.0978452316@gmail.com>
2024-05-25 16:53:51 +0700

    Edit : a little bit

--- DSA/Array/main.go
+++ DSA/Array/main.go
@@ -24,15 +24,18 @@ func deleteValue(myArray []int, value int) []int {
 }
 func displayValue(myArray []int) {
 	for i := 0; i < len(myArray); i++ {
-		fmt.Println(" Value : ", myArray[i])
+		fmt.Println(" Display : ", myArray[i])
 	}
 }
 func main() {
+	fmt.Println("Now Value in array is null and Add value in array")
 	myArray := []int{}
 	myArray = addValue(myArray, 112, 113, 114)
 	displayValue(myArray)
-	updateValue(myArray, 113, 118)
-	fmt.Println("......")
+	fmt.Println("Updated value")
+	myArray = updateValue(myArray, 113, 118)
+	displayValue(myArray)
+	fmt.Println("Delete value")
 	myArray = deleteValue(myArray, 112)
 	displayValue(myArray)
 }
