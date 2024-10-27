8b83d3c5b92a0722cf13139aa937ac4ba252e8ef
TaiChi <anothai.0978452316@gmail.com>
2024-10-27 16:40:19 +0700

    learn : shortcut declaration var in Go

--- project_name/main.go
+++ project_name/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
-	print("Runnig successfully")
-	print("Runnig successfully")
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+	fmt.Println("i=", i, "j=", j, "k=", k, "c=", c, "python=", python, "java=", java)
+
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
